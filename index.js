59781f73d681c591eaa39785660c4c6d7d193569
Luizinho101 <luiz.antonio.da.silva777@gmail.com>
2026-04-03 23:39:53 -0300

    feat: adição dos metodos Get e Post

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
-app.get('/', (req , res) => {
-    res.json({msg : 'olá , mundo'})
+let usuarios = {};
+let id = 1;
+
+app.get('/usuarios', (req, res) => {
+    res.json({msg: Object.values(usuarios)})
+})
+
+app.post('/usuarios', (req, res) => {
+    const usuario = req.body
+    const idUsuario = id;
+    usuario.id = idUsuario;
+    usuarios[idUsuario] = usuario;
+    id += 1;
+    res.json({msg: "Usuarios adicionado com sucesso!"})
 })
 
 app.listen(8080, () => {
